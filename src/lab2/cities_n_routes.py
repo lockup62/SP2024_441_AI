35e47420ab9e61de4f627541b0a3b657c66ad632
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-01-23 11:19:04 -0500

    Lab Finished cities_n_routes.py

--- src/lab2/cities_n_routes.py
+++ src/lab2/cities_n_routes.py
@@ -8,27 +8,15 @@ import random
 import itertools
 
 def get_randomly_spread_cities(size, n_cities):
-    """
-    > This function takes in the size of the map and the number of cities to be generated 
-    and returns a list of cities with their x and y coordinates. The cities are randomly spread
-    across the map.
-    
-    :param size: the size of the map as a tuple of 2 integers
-    :param n_cities: The number of cities to generate
-    :return: A list of cities with random x and y coordinates.
-    """
-    # Consider the condition where x size and y size are different
+    coord = []
+    for i in range(n_cities):
+        coord.append((random.randint(0, size[0]), random.randint(0, size[1])))
+    return coord
+
     pass
 
 def get_routes(city_names):
-    """
-    It takes a list of cities and returns a list of all possible routes between those cities. 
-    Equivalently, all possible routes is just all the possible pairs of the cities. 
-    
-    :param cities: a list of city names
-    :return: A list of tuples representing all possible links between cities/ pairs of cities, 
-            each item in the list (a link) represents a route between two cities.
-    """
+    return list(itertools.combinations(city_names, 2))
     pass
 
 
