c332959722d75c861e581fc8a5ee6cb20aae9b70
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-02-06 18:35:53 -0500

    Completed landscape.py
    Finished

--- src/lab5/landscape.py
+++ src/lab5/landscape.py
@@ -5,13 +5,21 @@ import numpy as np
 def get_elevation(size):
     xpix, ypix = size
     elevation = np.array([])
+    noise = (PerlinNoise(octaves = 20, seed =2305))
+    xpix,ypix = size[0], size[1]
+    print(size[0], size[1])
+    elevation = np.array([[noise([i/xpix, j/ypix])
+                           for j in range (ypix)]
+                         for i in range (xpix)]
+                        )
+  
     '''Play around with perlin noise to get a better looking landscape (This is required for the lab)'''
 
     return elevation
 
 def elevation_to_rgba(elevation):
     xpix, ypix = np.array(elevation).shape
-    colormap = plt.cm.get_cmap('gist_earth')
+    colormap = plt.cm.get_cmap('Greens')
     elevation = (elevation - elevation.min())/(elevation.max()-elevation.min())
     ''' You can play around with colormap to get a landscape of your preference if you want '''
     landscape = np.array([colormap(elevation[i, j])[0:3] for i in range(xpix) for j in range(ypix)]).reshape(xpix, ypix, 3)*255
