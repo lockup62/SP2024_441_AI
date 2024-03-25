088de6b59420690562c05948064cc2daec85fe0e
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-03-24 23:57:48 -0400

    Update pygame_ai_player.py

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,23 @@
-""" Create PyGameAIPlayer class here"""
-
+import pygame
+import random
+from lab11.turn_combat import CombatPlayer
 
 class PyGameAIPlayer:
-    pass
+    def __init__(self):
+        pass
 
+    def selectAction(self, state):
+        #this lets the ai move to each city as long as its not at the last city
+        if state.current_city == 10:
+            return None
+        else:
+            #this returns the ascii value of whatever the city index is at
+            return ord(str(state.current_city+1))
 
-""" Create PyGameAICombatPlayer class here"""
+class PyGameAICombatPlayer(CombatPlayer):
+    def __init__(self, name):
+        super().__init__(name)
 
-
-class PyGameAICombatPlayer:
-    pass
+    def weapon_selecting_strategy(self):
+        self.weapon_select=random.randint(0,2)
+        return self.weapon_select
