e3b4f8656685e45519f8ee318aae8b3b5ec40c25
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-03-26 11:00:22 -0400

    Completed episode.py

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -1,8 +1,6 @@
 ''' 
 Lab 12: Beginnings of Reinforcement Learning
-We will modularize the code in pygrame_combat.py from lab 11 together.
 
-Then it's your turn!
 Create a function called run_episode that takes in two players
 and runs a single episode of combat between them. 
 As per RL conventions, the function should return a list of tuples
@@ -11,3 +9,40 @@ Note that observation/state is a tuple of the form (player1_health, player2_heal
 Action is simply the weapon selected by the player.
 Reward is the reward for the player for that turn.
 '''
+def run_episode(player1, player2):
+    # Initialize combat environment
+    combat = Combat()
+
+    #Make list for storing state action reward tuples
+    episodes = []
+
+    while not combat.gameOver:
+        #get current state/health of both players
+        state = (player1.health, player2.health)
+
+        #player 1 turn
+        action_player1 = player1.selectAction(state)
+        combat.newRound()
+        combat.takeTurn(player1, player2)
+        reward_player1 = combat.checkWin(player1, player2)
+
+        #store state action reward for player 1
+        episodes.append((state, action_player1, reward_player1))
+
+        #if combats done
+        if combat.gameOver:
+            break
+
+        #get state/health again
+        state = (player1.health, player2.health)
+
+        #Player 2 turn
+        action_player2 = player2.selectAction(state)
+        combat.newRound()
+        combat.takeTurn(player2, player1)
+        reward_player2 = combat.checkWin(player2, player1)
+
+        #Store state action rewardfor player 2
+        episodes.append((state, action_player2, reward_player2))
+
+    return episodes
