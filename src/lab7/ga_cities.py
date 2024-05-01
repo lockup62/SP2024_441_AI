6b46d14b7c0be2ab0d5d3d941daec0114eb31b25
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-04-30 21:37:44 -0400

    FINISHED ga_cities.py

--- src/lab7/ga_cities.py
+++ src/lab7/ga_cities.py
@@ -1,172 +1,124 @@
-"""
-Lab 7: Realistic Cities 
-
-In this lab you will try to generate realistic cities using a genetic algorithm.
-Your cities should not be under water, and should have a realistic distribution across the landscape.
-Your cities may also not be on top of mountains or on top of each other.
-Create the fitness function for your genetic algorithm, so that it fulfills these criterion
-and then use it to generate a population of cities.
-
-Please comment your code in the fitness function to explain how are you making sure each criterion is 
-fulfilled. Clearly explain in comments which line of code and variables are used to fulfill each criterion.
-"""
-import matplotlib.pyplot as plt
-import pygad
-import numpy as np
-
 import sys
-from pathlib import Path
+import pygame
+from sprite import Sprite
+from pygame_combat import run_pygame_combat
+from pygame_human_player import PyGameHumanPlayer
+from landscape import get_landscape_surface, get_combat_surface
+from pygame_ai_player import PyGameAIPlayer
+from pygame_human_player import PyGameHumanCombatPlayer
+from ga_cities import generate_cities
 
-sys.path.append(str((Path(__file__) / ".." / ".." / "..").resolve().absolute()))
+pygame.font.init()
+game_font = pygame.font.SysFont("Comic Sans MS", 15)
 
-from src.lab5.landscape import elevation_to_rgba
-from src.lab5.landscape import get_elevation
+def setup_window(width, height, caption):
+    pygame.init()
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption(caption)
+    return window
 
+def displayCityNames(city_locations, city_names):
+    for i, name in enumerate(city_names):
+        text_surface = game_font.render(str(i) + " " + name, True, (0, 0, 150))
+        screen.blit(text_surface, city_locations[i])
 
-
-def game_fitness(solution, idx, elevation, size):
-    #fitness starts at 100
-    fitness = 100 
-
-    #Looping through every city
-    for city in solution_to_cities(solution, size):
-        #Getting the elevation of the current city from the elevation array
-        city_elevation = elevation[city[0], city[1]]
-
-        #adjusting highs and lows
-        if city_elevation > 0.9:
-             fitness -= 2
-        
-        if city_elevation < 0.1:
-            fitness -= 2
-         
-
-        if 0.4 < city_elevation and city_elevation < 0.6:
-            fitness += 2
-        
-        #Spacing distance for cities in the x and y directions
-        x= size[0]/10
-        y= size[1]/10
-
-        #Loop through each city and check proximity
-        for next_city in solution_to_cities(solution, size):
-            if (not np.array_equal(next_city, city) and
-                abs(next_city[0] - city[0]) <= x
-                and abs(next_city[1] - city[1]) <= y
-                
-            ):
-                fitness -= 3  #Cities too close so subtract fitness
-
-    return fitness
-
-
-def setup_GA(fitness_fn, n_cities, size):
-    """
-    It sets up the genetic algorithm with the given fitness function,
-    number of cities, and size of the map
-
-    :param fitness_fn: The fitness function to be used
-    :param n_cities: The number of cities in the problem
-    :param size: The size of the grid
-    :return: The fitness function and the GA instance.
-    """
-    num_generations = 100
-    num_parents_mating = 10
-
-    solutions_per_population = 300
-    num_genes = n_cities
-
-    init_range_low = 0
-    init_range_high = size[0] * size[1]
-
-    parent_selection_type = "sss"
-    keep_parents = 10
-
-    crossover_type = "single_point"
-
-    mutation_type = "random"
-    mutation_percent_genes = 10
-
-    ga_instance = pygad.GA(
-        num_generations=num_generations,
-        num_parents_mating=num_parents_mating,
-        fitness_func=fitness_fn,
-        sol_per_pop=solutions_per_population,
-        num_genes=num_genes,
-        gene_type=int,
-        init_range_low=init_range_low,
-        init_range_high=init_range_high,
-        parent_selection_type=parent_selection_type,
-        keep_parents=keep_parents,
-        crossover_type=crossover_type,
-        mutation_type=mutation_type,
-        mutation_percent_genes=mutation_percent_genes,
-    )
-
-    return fitness_fn, ga_instance
-
-
-def solution_to_cities(solution, size):
-    """
-    It takes a GA solution and size of the map, and returns the city coordinates
-    in the solution.
-
-    :param solution: a solution to GA
-    :param size: the size of the grid/map
-    :return: The cities are being returned as a list of lists.
-    """
-    cities = np.array(
-        list(map(lambda x: [int(x / size[0]), int(x % size[1])], solution))
-    )
-    return cities
-
-
-def show_cities(cities, landscape_pic, cmap="gist_earth"):
-    """
-    It takes a list of cities and a landscape picture, and plots the cities on top of the landscape
-
-    :param cities: a list of (x, y) tuples
-    :param landscape_pic: a 2D array of the landscape
-    :param cmap: the color map to use for the landscape picture, defaults to gist_earth (optional)
-    """
-    cities = np.array(cities)
-    plt.imshow(landscape_pic, cmap=cmap)
-    plt.plot(cities[:, 1], cities[:, 0], "r.")
-    plt.show()
-
+class State:
+    def __init__(
+        self,
+        current_city,
+        destination_city,
+        travelling,
+        encounter_event,
+        cities,
+        routes,
+    ):
+        self.current_city = current_city
+        self.destination_city = destination_city
+        self.travelling = travelling
+        self.encounter_event = encounter_event
+        self.cities = cities
+        self.routes = routes
 
 if __name__ == "__main__":
-    print("Initial Population")
+    size = width, height = 640, 480
+    black = 1, 1, 1
+    start_city = 0
+    end_city = 9
+    sprite_path = "assets/lego.png"
+    sprite_speed = 1
 
-    size = 100, 100
-    n_cities = 10
-    elevation = []
-    """ initialize elevation here from your previous code"""
-    # normalize landscape
-    theElevation = get_elevation(size)
-    elevation = (theElevation - theElevation.min()) / (theElevation.max() - theElevation.min())
-    landscape_pic = elevation_to_rgba(elevation)
+    screen = setup_window(width, height, "Game World Gen Practice")
 
-    # setup fitness function and GA
-    fitness = lambda cities, idx: game_fitness(
-        cities, idx, elevation=elevation, size=size
+    landscape_surface = get_landscape_surface(size)
+    combat_surface = get_combat_surface(size)
+    city_names = [
+        "Morkomasto",
+        "Morathrad",
+        "Eregailin",
+        "Corathrad",
+        "Eregarta",
+        "Numensari",
+        "Rhunkadi",
+        "Londathrad",
+        "Baernlad",
+        "Forthyr",
+    ]
+
+    # Generate cities using the GA system
+    cities, routes = generate_cities(size, len(city_names))
+
+    player_sprite = Sprite(sprite_path, cities[start_city])
+
+    player = PyGameHumanPlayer()
+
+    state = State(
+        current_city=start_city,
+        destination_city=start_city,
+        travelling=False,
+        encounter_event=False,
+        cities=cities,
+        routes=routes,
     )
-    fitness_function, ga_instance = setup_GA(fitness, n_cities, size)
 
-    # Show one of the initial solutions.
-    cities = ga_instance.initial_population[0]
-    cities = solution_to_cities(cities, size)
-    show_cities(cities, landscape_pic)
+    while True:
+        action = player.selectAction(state)
+        if 0 <= int(chr(action)) <= 9:
+            if int(chr(action)) != state.current_city and not state.travelling:
+                start = cities[state.current_city]
+                state.destination_city = int(chr(action))
+                destination = cities[state.destination_city]
+                player_sprite.set_location(cities[state.current_city])
+                state.travelling = True
+                print(
+                    "Travelling from", state.current_city, "to", state.destination_city
+                )
 
-    # Run the GA to optimize the parameters of the function.
-    ga_instance.run()
-    ga_instance.plot_fitness()
-    print("Final Population")
+        screen.fill(black)
+        screen.blit(landscape_surface, (0, 0))
 
-    # Show the best solution after the GA finishes running.
-    cities = ga_instance.best_solution()[0]
-    cities_t = solution_to_cities(cities, size)
-    plt.imshow(landscape_pic, cmap="gist_earth")
-    plt.plot(cities_t[:, 1], cities_t[:, 0], "r.")
-    plt.show()
-    print(fitness_function(cities, 0))
+        for city in cities:
+            pygame.draw.circle(screen, (255, 0, 0), city, 5)
+
+        for line in routes:
+            pygame.draw.line(screen, (255, 0, 0), *line)
+
+        displayCityNames(cities, city_names)
+        if state.travelling:
+            state.travelling = player_sprite.move_sprite(destination, sprite_speed)
+            state.encounter_event = random.randint(0, 1000) < 2
+            if not state.travelling:
+                print('Arrived at', state.destination_city)
+
+        if not state.travelling:
+            encounter_event = False
+            state.current_city = state.destination_city
+
+        if state.encounter_event:
+            run_pygame_combat(combat_surface, screen, player_sprite)
+            state.encounter_event = False
+        else:
+            player_sprite.draw_sprite(screen)
+        pygame.display.update()
+        if state.current_city == end_city:
+            print('You have reached the end of the game!')
+            break
